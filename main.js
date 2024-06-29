185d5afa8315fb2e05d2af93e6d946914c046dce
Miracle Micheal <135803949+Chukuameke@users.noreply.github.com>
2024-06-30 00:05:53 +0100

    dd

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const dropDown = document.getElementById("drop-down-menu");
+btn.addEventListener('click', ()=>{
+dropDown.classList.toggle("show");
+
+})
