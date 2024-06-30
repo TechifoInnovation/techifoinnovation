b7ce0e906a3a64ef90993d07ac5c78aebe9cde34
Miracle Micheal <135803949+Chukuameke@users.noreply.github.com>
2024-06-30 01:04:21 +0100

    sss

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const btn = document.getElementById("btn");
-const dropDown = document.getElementById("drop-down-menu");
-btn.addEventListener('click', ()=>{
-dropDown.classList.toggle("show");
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById("btn");
+    const dropDown = document.getElementById("drop-down-menu");
 
-})
+    btn.addEventListener('click', () => {
+        dropDown.classList.toggle("show");
+    });
+});
